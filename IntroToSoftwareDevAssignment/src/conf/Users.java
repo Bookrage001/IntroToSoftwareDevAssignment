70afefceb4a49f088903f6a59cdf7912daf54f53
13241464147 <49430046+13241464147@users.noreply.github.com>
2019-04-16 19:47:39 +1000

    Update Users.java

--- IntroToSoftwareDevAssignment/src/conf/Users.java
+++ IntroToSoftwareDevAssignment/src/conf/Users.java
@@ -4,10 +4,34 @@
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Supercheap Computer
+ * @author Mark Galulu
  */
 public class Users {
+
+    private ArrayList<User> list = new ArrayList<User>();
+
+    public Users() {
+    }
     
+    public Users(ArrayList<Users> list){
+        this.list = list;
+    }
+    
+    public ArrayList<Users> getList(){
+        return list;
+    }
+    
+    public Users getId(int id){
+        for(Users user : list){
+            if(Users.getUsername() == username){
+                return user;
+            }
+        }
+        return null;
+    } 
 }
+
