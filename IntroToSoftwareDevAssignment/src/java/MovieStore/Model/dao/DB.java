c3092322e2189372de5bcce280215f5d96cae00b
hclaud <49181186+hclaud@users.noreply.github.com>
2019-05-23 22:26:01 +1000

    :+1:  update logdb

--- IntroToSoftwareDevAssignment/src/java/MovieStore/Model/dao/DB.java
+++ IntroToSoftwareDevAssignment/src/java/MovieStore/Model/dao/DB.java
@@ -16,7 +16,7 @@ public abstract class DB {
 
     protected String URL = "jdbc:derby://localhost:1527/";
     protected String db = "logdb";
-    protected String dbuser = "Administrator";
+    protected String dbuser = "administrator";
     protected String dbpass = "admin";
     protected String driver = "org.apache.derby.jdbc.ClientDriver";
     protected Connection conn;
