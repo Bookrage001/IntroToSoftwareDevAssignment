5bfd0895dbcb47d42ac62ede3c167b4839d5cbe7
caleb.ardern <caleb.ardern@gmail.com>
2019-05-28 08:08:08 +1000

    can i get it to work again

--- IntroToSoftwareDevAssignment/src/java/MovieStore/controller/ConnServlet.java
+++ IntroToSoftwareDevAssignment/src/java/MovieStore/controller/ConnServlet.java
@@ -1,6 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package MovieStore.controller;
 
+import MovieStore.Model.dao.DBConnector;
+import MovieStore.Model.dao.DBManager;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -11,15 +19,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import MovieStore.Model.dao.*;
-
+/**
+ *
+ * @author mcant
+ */
 public class ConnServlet extends HttpServlet {
 
     private DBConnector db;
     private DBManager manager;
     private Connection conn;
 
-    @Override // Create and instance of DBConnector for the deployment session
+    @Override //Create and instance of DBConnector for the deployment session
     public void init() {
         try {
             db = new DBConnector();
@@ -28,24 +38,24 @@ public class ConnServlet extends HttpServlet {
         }
     }
 
-    @Override // Add the DBConnector, DBManager, Connection instances to the session
+    @Override //Add the DBManager instance to the session
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        HttpSession session = request.getSession();
         conn = db.openConnection();
+        HttpSession session = request.getSession();
         try {
             manager = new DBManager(conn);
         } catch (SQLException ex) {
             Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        session.setAttribute("db", db);
+        //export the DB manager to the view-session (JSPs)
         session.setAttribute("manager", manager);
-        session.setAttribute("conn", conn);
+
     }
 
-    @Override // Destroy the servlet and release the resources of the application
-    public void destroy() {
+    @Override //Destroy the servlet and release the resources of the application
+     public void destroy() {
         try {
             db.closeConnection();
         } catch (SQLException ex) {
